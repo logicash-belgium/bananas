d1a72bad75a8cfbf19d8d106e3c4da78081182ff
corentingoo <57222288+corentingoo@users.noreply.github.com>
2021-07-26 12:07:08 +0200

    delete commentaries

--- lib/src/Counter.dart
+++ lib/src/Counter.dart
@@ -1,7 +1,6 @@
 import 'package:rxdart/rxdart.dart';
 
 class Counter {
-  // TODO: convert to https://medium.com/flutter-community/why-use-rxdart-and-how-we-can-use-with-bloc-pattern-in-flutter-a64ca2c7c52d
 
   int _value = 0;
   int get value => _value;
