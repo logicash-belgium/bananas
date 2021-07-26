e98d444a1d5ef3d080420ef235cbe5523261e741
Guillaume Maiano <guillaume@maiano.fr>
2021-07-26 10:16:38 +0200

    Makes counters readonly, and builds a stream version.

--- lib/src/Counter.dart
+++ lib/src/Counter.dart
@@ -1,13 +1,26 @@
+import 'package:rxdart/rxdart.dart';
+
 class Counter {
   // TODO: convert to https://medium.com/flutter-community/why-use-rxdart-and-how-we-can-use-with-bloc-pattern-in-flutter-a64ca2c7c52d
 
-  int value = 0;
+  int _value = 0;
+  int get value => _value;
+  // Subjects are a type of Streams.
+  BehaviorSubject<int> _subjectCounter = BehaviorSubject<int>();
+
+  Stream get counterStream => _subjectCounter.stream;
 
    void increment() {
-    value++;
+    _value++;
+    _subjectCounter.sink.add(value);
    }
 
   void decrement() {
-    value--;
+    _value--;
+    _subjectCounter.sink.add(value);
+  }
+
+  void dispose() {
+     _subjectCounter.close();
   }
 }
